995db097fafca65ea1525eb73db185827536b9a2
bgoktugozdemir <bgoktugozdemir@gmail.com>
2018-12-04 14:40:29 +0300

    İlk Commit

---
+++ Hotel.BI/Repository/AnaSayfaYonetim.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hotel.BI.Interface;
+
+namespace Hotel.BI.Repository
+{
+    public class AnaSayfaYonetim: IAnaSayfaYonetim
+    {
+
+    }
+}
---
+++ WebProje/Models/Home/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Hotel.Model.DataModel;
+
+namespace WebProje.Models.Home
+{
+    public class HomeViewModel
+    {
+        public List<Otel> Oteller { get; set; }
+    }
+}
